0f9fca1a4b9470f15508a74fde2886f125ec6eb5
David Durbin <David-Durbin@users.noreply.github.com>
2017-09-09 15:05:01 -0500

    Add files via upload

---
+++ ddurbin_PythonPrintAssignment.py
@@ -0,0 +1,21 @@
+# David Durbin
+# Sept. 9, 2017
+# Printing Assignment (2.1)
+
+# The Fall of Gil-galad
+# J.R.R. Tolkien
+
+print("Gil-galad was an Even-king.")
+print("Of him the harpers sadly sing:")
+print("the last whose realm was fair and free")
+print("between the Mountains and the Sea.\n")
+
+print("His sword was long, his lance was keen,")
+print("his shining helm afar was seen;")
+print("the countless stars of heaven's field")
+print("were mirrored in his silver shield.\n")
+
+print("But long ago he rode away,")
+print("and where he dwelleth none can say;")
+print("for into darkness fell his star")
+print("in Mordor where the shadows are.")
